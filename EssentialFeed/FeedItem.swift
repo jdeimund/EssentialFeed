c242bf8df0431f64155e4323e2f0313844a9ce72
Jeff Deimund <jdeimund@icloud.com>
2022-08-09 19:34:13 -0500

    Create feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jeff Deimund on 8/9/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
